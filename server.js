e069e88b06641178ef41f21e5a1866c4c6a2bf2e
Casey Tokarchuk <105597527+toka1300@users.noreply.github.com>
2023-11-03 08:09:13 -0400

    Final updates

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var cors = require('cors');
-var axios = require('axios');
+import express from 'express';
+const app = express();
+
+import bodyParser from 'body-parser';
+import cors from 'cors'
+import axios from 'axios'
 
 app.use(cors());
 app.use(bodyParser.json());
 
 //Create your endpoints HERE
+let dogPhoto
+try {
+  const response = await axios.get('https://dog.ceo/api/breeds/image/random')
+  dogPhoto = response.data.message
+} catch (err) {
+  console.log('Error:', err);
+}
 
 app.listen(8001, function () {
   console.log('App running on port 8001');
 });
+
+app.get('/', (req, res) => {
+  console.log(dogPhoto);
+  res.render('generateImage')
+})
